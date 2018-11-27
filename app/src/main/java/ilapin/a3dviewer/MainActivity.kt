86813879a953ad3de5d8415445d8e075e98e291f
Igor Lapin <raynor73@gmail.com>
2018-11-27 09:33:00 +0300

    Setup full screen mode

--- app/src/main/java/ilapin/a3dviewer/MainActivity.kt
+++ app/src/main/java/ilapin/a3dviewer/MainActivity.kt
@@ -1,12 +1,56 @@
 package ilapin.a3dviewer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var isFullscreen = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        container.setOnClickListener { toggleFullscreen() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (isFullscreen) {
+            hideControls()
+        }
+    }
+
+    private fun toggleFullscreen() {
+        if (isFullscreen) {
+            showControls()
+        } else {
+            hideControls()
+        }
+    }
+
+    private fun showControls() {
+        isFullscreen = false
+        window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        controls.animate().alpha(1f).start()
+    }
+
+    private fun hideControls() {
+        isFullscreen = true
+        window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        controls.animate().alpha(0f).start()
     }
 }
