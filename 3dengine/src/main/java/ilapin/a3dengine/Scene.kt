e52209b79ec6bcac8c7b3152f34ce580be483ca6
Igor Lapin <raynor73@gmail.com>
2018-12-12 00:17:24 +0300

    Introduce Viewer Scene, patch #2

--- 3dengine/src/main/java/ilapin/a3dengine/Scene.kt
+++ 3dengine/src/main/java/ilapin/a3dengine/Scene.kt
@@ -1,8 +1,17 @@
 package ilapin.a3dengine
 
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
 open class Scene {
 
+    private val sceneObjectAddedSubject = PublishSubject.create<SceneObject>()
+    private val sceneObjectRemovedSubject = PublishSubject.create<SceneObject>()
+
     var rootObject: SceneObject? = null
+
+    val sceneObjectAdded: Observable<SceneObject> = sceneObjectAddedSubject
+    val sceneObjectRemoved: Observable<SceneObject> = sceneObjectRemovedSubject
 
     fun update() {
         rootObject?.update()
